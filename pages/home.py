9e1e93c5be0089fb864f618c85286f0a02a9f549
marco-is-a-student <145327911+marco-is-a-student@users.noreply.github.com>
2026-02-05 10:43:19 +0800

    added some stuff

--- pages/home.py
+++ pages/home.py
@@ -3,19 +3,19 @@ import auth_functions
 
 
 
-# Show user information
-st.header('User information:')
 try:
-    st.write(st.session_state.user_info)
+    var = st.session_state.user_info
 except Exception:
     st.switch_page('app.py')
     
 st.balloons()
-# Sign out
-st.header('Sign out:')
-st.button(label='Sign Out',on_click=auth_functions.sign_out,type='primary')
 
-# Delete Account
-st.header('Delete account:')
-password = st.text_input(label='Confirm your password',type='password')
-st.button(label='Delete Account',on_click=auth_functions.delete_account,args=[password],type='primary')
+
+
+
+st.header("Welcome, user")
+
+if st.button("Go to settings"):
+    st.switch_page("./pages/settings.py")
+
+
