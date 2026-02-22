ed995d61650b8fea758d75a6e91dc698149b5a7c
Matthias Ang <ang_sen_luen_matthias@s2023.ssts.edu.sg>
2026-02-22 22:18:51 +0800

    oh

--- pages/interface_page.py
+++ pages/interface_page.py
@@ -5,7 +5,7 @@ import random
 import json
 from backend import marco_invoke_qa_agent
 from pages.temppage2 import make_nice_task
-import firestore
+import firestore_functions
 from auth_functions import getUserId
 
 st.set_page_config(page_title="Interface",layout='wide')
@@ -85,4 +85,4 @@ with col2:
                 newstring = json.loads(string)[0]
                 print("AFTER", newstring)
                 name = newstring.pop("name")
-                firestore.writeorupdateDocument("users", getUserId(), newstring, "studyplans", name)
+                firestore_functions.writeorupdateDocument("users", getUserId(), newstring, "studyplans", name)
